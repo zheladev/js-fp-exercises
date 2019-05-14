68e58b9f23d98832e1eaebc6775d040a27d48c59
aitorsantillana <aitor.santillana@labela.nl>
2019-05-14 10:49:48 +0200

    Initial commit

---
+++ lib.js
@@ -0,0 +1,34 @@
+//return array with arguments passed
+module.exports = clist = (...args) => {
+    return args;
+}
+
+//return sum of args
+module.exports = add = (...args) => {
+    return args.length > 1 ?
+        args.reduce((n, m) => (n + m)) :
+        args[0].reduce((n, m) => (n + m));
+}
+
+//return substraction of args from left to right (first one being the number to substract from)
+module.exports = sub = (...args) => {
+    return args.length > 1 ?
+        args.reduce((n, m) => (n - m)) :
+        args[0].reduce((n, m) => (n - m));
+}
+
+module.exports = compose = (...fn) => (...n) => {
+    return fn.reverse().reduce((acc, curr) => curr(acc), n);
+}
+
+module.exports = transposenxn = (...n) => {
+    return n.reduce((acc, _, i, arr) => (acc[i] = arr.map((m) => m[i]), acc), Array(n.length))
+}
+
+module.exports = transpose = (...n) => {
+    return n[0].map((_, i) => n.map((m) => m[i]))
+}
+
+module.exports = zipmap = (...n) => {
+    return n[0].reduce((acc, curr, i) => (acc[curr] = n[1][i], acc), {})
+}
---
+++ test.js
@@ -0,0 +1,23 @@
+var l = require('./lib');
+
+console.log(add(1, 2, 3, 4, 5, 6, 8))
+console.log(sub(9, 5, 2))
+
+const double = x => 2 * x
+const negate = x => x * -1
+const fn = compose(negate, double, double, add)
+console.log(fn(1, 1, 1, 1))
+
+const bigArr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12],
+    [13, 14, 15],
+    [16, 17, 18],
+    [19, 20, 21]
+]
+
+console.log(transpose(...bigArr))
+
+console.log(zipmap(["a", "b", "c"], [1, 2, 3]))
